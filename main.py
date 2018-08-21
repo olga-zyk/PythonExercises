fb2877662c46775ee86495e6bdc14adb6e1fc927
Olga Zyk <my.agleh@gmail.com>
2018-08-21 11:47:06 +0300

    Add functions that print a tree;

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from elves import Elves
+from tree import ChristmasTree
 
 config = {'file_name': 'elves.json'}
 
 
 elves = Elves(config)
 elves.write_report()
+
+tree = ChristmasTree()
+tree.print_tree_method_one()
+tree.print_tree_method_two()
+tree.print_tree_method_three()
--- tree.py
+++ tree.py
@@ -13,3 +13,29 @@
 # ***
 
 # Your code here
+
+
+class ChristmasTree():
+
+    def print_tree_method_one(self):
+        height = int(input('Type in the height of the tree: '))
+
+        for i in range(1, height + 1):
+            print('*' * i)
+
+    def print_tree_method_two(self):
+        height = int(input('Type in the height of the tree: '))
+
+        for row in range(1, height + 1):
+            print(' ' * (height - row) + '* ' * row)
+
+    def print_tree_method_three(self):
+        height = int(input('Type in the height of the tree: '))
+
+        completed_row = ''
+        for row in range(1, height + 1):
+            space = ' ' * (height - row)
+            for star in range(1, height + 1):
+                stars = '* ' * row
+                completed_row = space + stars
+            print(completed_row)
