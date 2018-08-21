6745e59676479d5ec22c0f96eb5c42924377e8b4
Olga Zyk <my.agleh@gmail.com>
2018-08-21 09:43:50 +0300

    Add class Elves; Add I/O functions;

--- elves.py
+++ elves.py
@@ -4,3 +4,43 @@
 # An example report is displayed in `elves_sample.txt`
 
 # Your code here
+import json
+
+
+class Elves:
+
+    def __init__(self, config):
+        self.report_file = 'elves.txt'
+        self.data = None
+
+        self.load(config['file_name'])
+
+    def load(self, filename):
+        try:
+            with open(filename, 'r') as json_file:
+                self.data = json.load(json_file)
+        except IOError as io_error:
+            print 'Error loading file. {0}'.format(io_error)
+        except ValueError as val_error:
+            print 'File decoding error. {0}'.format(val_error)
+
+    def write_report(self):
+        if self.data is None:
+            return
+
+        data_report = []
+        self.format_elves(data_report)
+
+        try:
+            with open(self.report_file, 'w+') as report:
+                report.write('\n'.join(data_report))
+        except IOError as io_error:
+            print('Data could not be written to file. {error}'.format(error=io_error))
+        else:
+            print 'Report was successfully saved to "{file_name}" file'.format(file_name=self.report_file)
+
+    def format_elves(self, data_report):
+        for elf in self.data['elves']:
+            data_report.append(
+                '=========\nElf: {elf[name]} {elf[surname]}\nQuota: {elf[quota]}'.format(elf=elf)
+            )
---
+++ main.py
@@ -0,0 +1,7 @@
+from elves import Elves
+
+config = {'file_name': 'elves.json'}
+
+
+elves = Elves(config)
+elves.write_report()
