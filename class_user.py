d7496111fbb629b29aa0703bb9d00cca8a14cde0
Olga Zyk <my.agleh@gmail.com>
2018-08-21 09:47:13 +0300

    Add class User;

--- class_user.py
+++ class_user.py
@@ -2,6 +2,21 @@
 
 # Your code here
 
+
+class User:
+
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def get_name(self):
+        return '{self.name} {self.surname}'.format(self=self)
+
+    def get_age(self):
+        return 'I am {self.age} years old'.format(self=self)
+
+
 user = User('John', 'Doe', 23)
 assert user.get_name() == 'John Doe'
 assert user.get_age() == 'I am 23 years old'
