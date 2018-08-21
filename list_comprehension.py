c0912b9e349079f615c2e8ea75d5f574d3c54d02
Olga Zyk <my.agleh@gmail.com>
2018-08-21 09:55:45 +0300

    Add function `create_array`;

--- list_comprehension.py
+++ list_comprehension.py
@@ -2,5 +2,10 @@
 
 # Your code here
 
+
+def create_array(p):
+    return [(i, pow(i, 3)) for i in range(1, p + 1)]
+
+
 assert create_array(2) == [(1, 1), (2, 8)]
 assert create_array(3) == [(1, 1), (2, 8), (3, 27)]
